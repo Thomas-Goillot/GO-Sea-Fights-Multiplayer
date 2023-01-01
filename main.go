3cabeddb792edf30ccabd827860950b5d478c31c
Thomas <222teegee@gmail.com>
2023-01-01 23:07:32 +0100

    multiplayer is starting to take place

--- main.go
+++ main.go
@@ -2,9 +2,21 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
+	"log"
 	"net/http"
+	"net/url"
 	"strconv"
+	"strings"
 )
+
+type Player struct {
+	ID    int
+	IP    string
+	Port  string
+	Name  string
+	Board Board
+}
 
 type Board struct {
 	/*
@@ -25,11 +37,12 @@ func (b *Board) InitBoard() {
 			b.Board[i][j] = "0"
 		}
 	}
-
 }
 
 func (b *Board) InitBoat() {
 	//init boats randomly
+
+	//definir une orientation
 	b.Boats[0] = [4]int{0, 0, 0, 1}
 	b.Boats[1] = [4]int{1, 0, 1, 2}
 	b.Boats[2] = [4]int{2, 0, 2, 3}
@@ -66,43 +79,33 @@ func (b *Board) Hit(w http.ResponseWriter, req *http.Request) {
 	case http.MethodPost:
 		if err := req.ParseForm(); err != nil {
 			w.WriteHeader(http.StatusBadRequest)
-			fmt.Println("Something went bad")
 			fmt.Fprintln(w, "Something went bad")
 			return
 		}
 
-		for key, value := range req.PostForm {
-			fmt.Println(key, "=>", value)
-		}
-
 		if _, ok := req.PostForm["x"]; !ok {
-			fmt.Println("Position x is missing")
 			fmt.Fprintln(w, "Position x is missing")
 			return
 		}
 
 		if _, ok := req.PostForm["y"]; !ok {
-			fmt.Println("Position y is missing")
 			fmt.Fprintln(w, "Position y is missing")
 			return
 		}
 
 		x, err := strconv.Atoi(req.PostForm["x"][0])
 		if err != nil {
-			fmt.Println("Position x is not a number")
 			fmt.Fprintln(w, "Position x is not a number")
 			return
 		}
 
 		y, err := strconv.Atoi(req.PostForm["y"][0])
 		if err != nil {
-			fmt.Println("Position y is not a number")
 			fmt.Fprintln(w, "Position y is not a number")
 			return
 		}
 
 		if x < 0 || x > 9 || y < 0 || y > 9 {
-			fmt.Println("Position out of the board")
 			fmt.Fprintln(w, "Position out of the board")
 			return
 		}
@@ -112,12 +115,10 @@ func (b *Board) Hit(w http.ResponseWriter, req *http.Request) {
 			for i := 0; i < len(b.Boats); i++ {
 				if x >= b.Boats[i][0] && x <= b.Boats[i][2] && y >= b.Boats[i][1] && y <= b.Boats[i][3] {
 					b.Board[x][y] = "2"
-					fmt.Println("Hit")
 					fmt.Fprintln(w, "Hit")
 
 					if b.CheckBoatSunk(x, y) {
 						b.NbBoatsLeft--
-						fmt.Println("Boat sunk")
 						fmt.Fprintln(w, "Boat sunk")
 
 						for j := b.Boats[i][0]; j <= b.Boats[i][2]; j++ {
@@ -132,22 +133,16 @@ func (b *Board) Hit(w http.ResponseWriter, req *http.Request) {
 			}
 
 			b.Board[x][y] = "1"
-			fmt.Println("Miss")
 			fmt.Fprintln(w, "Miss")
 
 		} else if b.Board[x][y] == "1" || b.Board[x][y] == "2" {
-			fmt.Println("Already hit")
 			fmt.Fprintln(w, "Already hit")
 		} else {
-			fmt.Println("Something went bad")
 			fmt.Fprintln(w, "Something went bad")
 		}
 
-		fmt.Fprintln(w, b.CheckWin())
-
 	default:
 		w.WriteHeader(http.StatusMethodNotAllowed)
-		fmt.Println("Method not allowed")
 		fmt.Fprintln(w, "Method not allowed")
 	}
 }
@@ -169,7 +164,6 @@ func (b *Board) CheckBoatSunk(x int, y int) bool {
 }
 
 func (b *Board) CheckWin() bool {
-	//check if all boats are sunk
 	return b.NbBoatsLeft == 0
 }
 
@@ -188,7 +182,7 @@ func ConvertToChar(value string) string {
 	}
 }
 
-func DisplayBoard(text string) {
+func ConvertStringToBoard(text string) [10][10]string {
 	var board [10][10]string
 	var k int
 	for i := 0; i < 10; i++ {
@@ -197,6 +191,10 @@ func DisplayBoard(text string) {
 			k++
 		}
 	}
+	return board
+}
+
+func (p *Player) DisplayBoard() {
 
 	fmt.Print("    ")
 	for i := 0; i < 10; i++ {
@@ -207,7 +205,7 @@ func DisplayBoard(text string) {
 	for i := 0; i < 10; i++ {
 		fmt.Print(i, " | ")
 		for j := 0; j < 10; j++ {
-			fmt.Print(ConvertToChar(board[i][j]), " | ")
+			fmt.Print(ConvertToChar(p.Board.Board[i][j]), " | ")
 		}
 		fmt.Print(i)
 		fmt.Println()
@@ -224,23 +222,165 @@ func DisplayBoard(text string) {
 	fmt.Println()
 }
 
+func (p *Player) SendName(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, p.Name)
+}
+
+func (p *Player) GetName(id int) {
+	//make a http request to the server to get the name of the player
+	resp, err := http.Get("http://" + p.IP + ":" + p.Port + "/name")
+	if err != nil {
+		fmt.Println("Error while getting the name of the player")
+	}
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		fmt.Println("Error while reading the name of the player")
+	}
+
+	//set the name and id of the player
+	p.Name = string(body)
+	p.ID = id
+}
+
+func (p *Player) DisplayName() {
+	fmt.Printf("- %s (id: %d)\n", strings.Replace(p.Name, "\n", "", -1), p.ID)
+}
+
+func (p *Player) GetBoard() {
+	//make a http request to the server to get the board of the player
+	resp, err := http.Get("http://" + p.IP + ":" + p.Port + "/board")
+	if err != nil {
+		fmt.Println("Error while getting the board of the player")
+	}
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		fmt.Println("Error while reading the board of the player")
+	}
+
+	//set the board of the player
+	p.Board.Board = ConvertStringToBoard(string(body))
+}
+
+func (p *Player) GetNbBoats() {
+	//make a http request to the server to get the number of boats of the player
+	resp, err := http.Get("http://" + p.IP + ":" + p.Port + "/boats")
+	if err != nil {
+		fmt.Println("Error while getting the number of boats of the player")
+	}
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		fmt.Println("Error while reading the number of boats of the player")
+	}
+
+	//set the number of boats of the player
+	p.Board.NbBoatsLeft, _ = strconv.Atoi(string(body))
+}
+
 func main() {
 
-	//init the board
-	var board Board
-	board.InitBoard()
-
-	//add boat to the board
-	board.InitBoat()
-
-	var text = "3301000000222000000022221000002222210000222222000000000000000000000000000000000000000000000000000000"
-	DisplayBoard(text)
+	var player Player
+	var players []Player
+	var addPlayer string
+	var idPlayer int
 
 	//start the server
-	http.HandleFunc("/board", board.SendBoard)
-	http.HandleFunc("/boats", board.NbBoats)
-	http.HandleFunc("/hit", board.Hit)
+	http.HandleFunc("/board", player.Board.SendBoard)
+	http.HandleFunc("/boats", player.Board.NbBoats)
+	http.HandleFunc("/hit", player.Board.Hit)
+	http.HandleFunc("/name", player.SendName)
 
-	http.ListenAndServe(":8080", nil)
+	//init the board
+	player.Board.InitBoard()
 
+	//add boat to the board
+	player.Board.InitBoat()
+
+	//Demander le port sur lequel le serveur va écouter
+	fmt.Print("Entrer le port sur lequel le serveur va écouter : ")
+	fmt.Scan(&player.Port)
+
+	//demander le nom du joueur
+	fmt.Print("Entrer votre nom : (Il sera affiché sur l'écran des autres joueurs) ")
+	fmt.Scan(&player.Name)
+
+	//start a goroutine
+	go func() {
+		log.Fatal(http.ListenAndServe(":"+player.Port, nil))
+	}()
+	for {
+
+		for addPlayer != "n" {
+			var player Player
+			fmt.Print("Entrer l'adresse ip du joueur : ")
+			fmt.Scan(&player.IP)
+			fmt.Print("Entrer le port du joueur : ")
+			fmt.Scan(&player.Port)
+			players = append(players, player)
+
+			//verifier que le joueur n'est pas dans la liste des joueurs
+			for i := 0; i < len(players)-1; i++ {
+				if players[i].IP == players[len(players)-1].IP && players[i].Port == players[len(players)-1].Port {
+					fmt.Println("Ce joueur est déjà dans la liste des joueurs")
+					players = players[:len(players)-1]
+				}
+			}
+
+			//verifier que le serveur est bien lancé
+			resp, err := http.Get("http://" + players[len(players)-1].IP + ":" + players[len(players)-1].Port + "/name")
+			if err != nil {
+				fmt.Println("Le Joueur n'est pas connecté")
+				players = players[:len(players)-1]
+			}
+			defer resp.Body.Close()
+
+			//demander si on veut ajouter un autre joueur
+			fmt.Print("Voulez vous ajouter un autre joueur ? (y/n) ")
+			fmt.Scan(&addPlayer)
+		}
+
+		//On recupere le nom des joueurs
+		for i := 0; i < len(players); i++ {
+			players[i].GetName(i)
+		}
+
+		//afficher le nom des joueurs
+		fmt.Println("Joueurs disponible : ")
+		for i := 0; i < len(players); i++ {
+			players[i].DisplayName()
+		}
+
+		//choisir contre qui jouer
+		fmt.Print("Entrer l'id du joueur contre qui vous voulez jouer : ")
+		fmt.Scan(&idPlayer)
+
+		//On recupere le nombre de bateaux du joueur
+		players[idPlayer].GetNbBoats()
+		//On affiche le nombre de bateaux du joueur
+		fmt.Println("Il reste", players[idPlayer].Board.NbBoatsLeft, "bateaux à couler ! Sur la grille de", players[idPlayer].Name, "")
+
+		//afficher le board du joueur
+		players[idPlayer].GetBoard()
+		players[idPlayer].DisplayBoard()
+
+		//Demander les coordonnées de la case à attaquer
+		var x, y int
+		fmt.Print("Entrer les coordonnées de la case à attaquer : ")
+		fmt.Scan(&x, &y)
+
+		//envoyer les coordonnées de la case à attaquer
+		resp, err := http.PostForm("http://"+players[idPlayer].IP+":"+players[idPlayer].Port+"/hit", url.Values{"x": {strconv.Itoa(x)}, "y": {strconv.Itoa(y)}})
+		if err != nil {
+			fmt.Println("Error while sending the hit")
+		}
+
+		//recuperer le resultat de l'attaque
+		body, err := ioutil.ReadAll(resp.Body)
+		if err != nil {
+			fmt.Println("Error while reading the hit")
+		}
+		fmt.Println(string(body))
+	}
 }
